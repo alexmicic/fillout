bdafde7935a37d829fd17f9a163f433dfff485b1
Alex M <alexander.micic@gmail.com>
2025-06-26 18:53:52 +0200

    Clean

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import FormBuilder from "@/pages/form-builder";
 
@@ -10,7 +8,6 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={FormBuilder} />
-      <Route path="/form-builder" component={FormBuilder} />
       <Route component={NotFound} />
     </Switch>
   );
@@ -19,10 +16,7 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
+      <Router />
     </QueryClientProvider>
   );
 }
